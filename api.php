f95b15ef1859bf4cf4077905542d7f427d4a214a
Saidur Rahman <saidurwd@gmail.com>
2023-09-24 00:23:34 +0600

    Attendance upload - API

---
+++ api.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Attendance upload - API</title>
+</head>
+
+<body>
+	<?php include 'cors.php'; ?>
+	<div class="body-content" style="max-width: 30%;margin: 0 auto">
+		<h3 id="timer_show"></h3>
+	</div>
+	<script type="text/javascript" src="JQuery3.2.1.min.js"></script>
+	<script type="text/javascript">
+		var counter = 5;
+		intID = setInterval(Gettime, 1000);
+
+		function Gettime() {
+			document.getElementById("timer_show").innerHTML = 'Next Upload After = ' + counter;
+			--counter
+			if (counter === 0) {
+				/*----------if time is out then call the function-------------*/
+				callFunction();
+			}
+		}
+
+		function callFunction() {
+			$.get("api_data.php", function(response, status) {
+				//console.log(response)
+				var data = JSON.parse(response);
+				//console.log(data);
+				$.ajax({
+					url: "http://localhost:8888/peoplepro-v1.2.3/api/v1/create",
+					method: "POST", //POST / GET
+					//dataType: "jsonp",
+                    // headers: {
+                    //     'X-CSRF-TOKEN': 'm3tX8JjVXsWhSd31Y9esq1S4yBbaPz17sHVWzlBH'
+                    // },
+					data: {
+						data: data
+					},
+					//crossDomain:true,
+					success: function(data) {
+						console.log(data);
+					}
+				});
+			});
+			/*--------start counter-----------*/
+			counter = 3600;
+		}
+	</script>
+</body>
+
+</html>
---
+++ api_data.php
@@ -0,0 +1,16 @@
+<?php
+    date_default_timezone_set("Asia/Dhaka");
+    require 'dbconn_mysql.php';
+
+    $sql_data  = "SELECT U.`Badgenumber` AS employee_id, DATE_FORMAT(C.CHECKTIME, '%d-%m-%Y') AS attendance_date, MIN(DATE_FORMAT(C.CHECKTIME, '%H:%i:%s')) AS clock_in, MAX(DATE_FORMAT(C.CHECKTIME, '%H:%i:%s')) AS clock_out, 'Add' AS 'action'
+    FROM CHECKINOUT  C
+    LEFT JOIN USERINFO U ON U.USERID=C.USERID
+    GROUP BY DATE_FORMAT(C.CHECKTIME, '%Y-%m-%d'), U.`Badgenumber` ORDER BY C.CHECKTIME DESC, U.`Badgenumber` ASC LIMIT 2";
+    $result = $conn->query($sql_data);
+	
+    $response = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $response[] = $row;
+    }
+    echo json_encode($response);
